ecd85e79651b80471bc2d575b5f3f08f672f658e
priel gozlan <prielgozlan@gmail.com>
2024-04-09 19:13:47 +0300

    priel

--- js/page2.js
+++ js/page2.js
@@ -2,41 +2,52 @@ let list_date = JSON.parse(localStorage.getItem("list_date"));
 if (!list_date) {
     list_date = []
 }
-let all_data = JSON.parse(localStorage.getItem("all_data"));
-if (!all_data) {
-    all_data = []
-}
-let list_date2 = JSON.parse(localStorage.getItem("list_date"));
+let list_date2 = JSON.parse(localStorage.getItem("list_date2"));
 if (!list_date2) {
     list_date2 = []
 }
-let all_data2 = JSON.parse(localStorage.getItem("all_data2"));
-if (!all_data2) {
-    all_data2 = []
+let list_h1 = JSON.parse(localStorage.getItem("list_h1"));
+if (!list_h1) {
+    list_h1 = []
+}
+let list_n1 = JSON.parse(localStorage.getItem("list_n1"));
+if (!list_n1) {
+    list_n1 = []
+}
+let list_h2 = JSON.parse(localStorage.getItem("list_h2"));
+if (!list_h2) {
+    list_h2 = []
+}
+let list_n2 = JSON.parse(localStorage.getItem("list_n2"));
+if (!list_n2) {
+    list_n2 = []
 }
 
 
-let list_h = [];
-let list_m = [];
-let list_s = [];
+
 let btn_1 = document.querySelector("#id_b1");
 let btn_2 = document.querySelector("#id_b2");
 let pr = document.querySelector("#id_p");
 
-let datesList = []
+
 window.onload = function(){
     refresh()
-    refresh2()
-}
-function refresh(){
-    for (let i = 0; i < all_data.length; i++) {
-        pr.innerHTML += all_data[i]
-    }
     
 }
-function refresh2(){
-    for (let i = 0; i < all_data2.length; i++) {
-        pr.innerHTML += all_data2[i]
+function refresh(){
+    pr.innerHTML = ""
+    
+    for (let i = 0; i < list_date.length; i++) {
+        pr.innerHTML += `${list_date[i]} ${list_h1[i]}.${list_n1[i]} <button id="id_b2" class="box_b1">שינוי כניסה</button>`
+        if (list_h2[i] == undefined){
+            pr.innerHTML += ""
+        }
+        else{
+            let sum_time_h = list_h2 - list_h1
+            let sum_time_n = list_n2 - list_n1
+            pr.innerHTML += ` ${list_h2[i]}.${list_n2[i]} <button id="id_b2" class="box_b1">שינוי יציאה</button> סה"כ שעות: ${sum_time_h} סה"כ דקות: ${sum_time_n}`
+        }
+        
     }
     
 }
@@ -45,31 +56,36 @@ btn_2.addEventListener("click", appendEventt2);
 
 function appendEvent1() {
     let date = new Date()
-    if (list_date.includes(date.getDate()) == false) {
-        let all_data1 = `${date.getDate()}/${date.getMonth() + 1}    ${date.getHours()}.${date.getMinutes()}`
-        list_date.push(date.getDate())
-        all_data.push(all_data1)
+    if (list_date.includes(`${date.getDate()}/${date.getMonth()}`) == false) {
+        // let all_data1 = `${date.getDate()}/${date.getMonth() + 1}    ${date.getHours()}.${date.getMinutes()}    <button id="id_b2" class="box_b1">שינוי כניסה</button>`
+        list_date.push(`${date.getDate()}/${date.getMonth()}`)
+        list_h1.push(date.getHours())
+        list_n1.push(date.getMinutes())
         refresh()
 
         
     }
 
     localStorage.setItem("list_date", JSON.stringify(list_date))
-    localStorage.setItem("all_data", JSON.stringify(all_data))
+    localStorage.setItem("list_h1", JSON.stringify(list_h1))
+    localStorage.setItem("list_n1", JSON.stringify(list_n1))
 }
 function appendEventt2() {
     let date = new Date()
-    if (list_date2.includes(date.getDate()) == false) {
-        let all_data3 = ` ${date.getHours()}.${date.getMinutes()}<br>`
-        list_date2.push(date.getDate())
-        all_data2.push(all_data3)
-        refresh2()
+    if (list_date2.includes(`${date.getDate()}/${date.getMonth()}`) == false) {
+        // let all_data3 = ` ${date.getHours()}.${date.getMinutes()}  <button id="id_b2" class="box_b1">שינוי יציאה</button>
+        // <h2 id="id_p"></h2><br> `
+        list_date2.push(`${date.getDate()}/${date.getMonth()}`)
+        list_h2.push(date.getHours())
+        list_n2.push(date.getMinutes())
+        
+        refresh()
         
     }
 
-    
     localStorage.setItem("list_date2", JSON.stringify(list_date2))
-    localStorage.setItem("all_data2", JSON.stringify(all_data2))
+    localStorage.setItem("list_h2", JSON.stringify(list_h2))
+    localStorage.setItem("list_n2", JSON.stringify(list_n2))
 
 
 
